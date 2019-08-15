fefd55285b4e5604af4cd50bfa7d0a2ec71f2381
nikugr <54131805+nikugr@users.noreply.github.com>
2019-08-15 15:02:53 +0300

    All cells change on every click

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "./styles.css";
 
 const main = () => {
+  let board = document.getElementById("board");
 
+  document.querySelectorAll("td").forEach((e, i) => {
+    e.onclick = () => {handleClick()};
+  });
+}
+
+const handleClick = () => {
+  document.querySelectorAll("td").forEach((e, i) => {
+    e.innerHTML = e.innerHTML === "x" ? "o" : "x";
+  });
 }
 
 export default main;
