2110f792a27925fe5bcd55d1d9ce08d8f63be891
nikugr <54131805+nikugr@users.noreply.github.com>
2019-08-15 15:07:07 +0300

    Added counted alerts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+var clickCount = 0;
+
 const main = () => {
   let board = document.getElementById("board");
 
@@ -12,6 +14,13 @@ const handleClick = () => {
   document.querySelectorAll("td").forEach((e, i) => {
     e.innerHTML = e.innerHTML === "x" ? "o" : "x";
   });
+  clickCount++;
+  if(clickCount === 9) {
+    alert("Player 1 won!");
+  }
+  else if(clickCount === 10) {
+    alert("Player 2 won!");
+  }
 }
 
 export default main;
